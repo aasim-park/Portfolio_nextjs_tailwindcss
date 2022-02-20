98beaafac96170e0e5d2445e9430ad0ab2d1fbc6
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-02-20 22:16:49 +0530

    Added Footer Navbar Contatcs and same updated in index

---
+++ Components/Navbar.tsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <div>
+        <a>Logo</a>
+      </div>
+      <a>Home</a>
+      <a>Projects</a>
+      <a>Contact</a>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ pages/Contact.tsx
@@ -0,0 +1,13 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1 className="font-bold">About</h1>
+      <p>
+        Hello this is Aasim. Hope you are having a wonderful daycheck below link
+        so we can collaborate.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
