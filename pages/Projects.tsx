e570c4268015cc7178cb173c1bee1c22506ce7a4
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-02-21 08:15:57 +0530

    Added head component

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -1,8 +1,15 @@
+import Head from "next/head";
+
 const Projects = () => {
   return (
-    <div>
-      <p>Here are some of my projects</p>
-    </div>
+    <>
+      <Head>
+        <title>Portfolio | Projects</title>
+      </Head>
+      <div>
+        <p>Here are some of my projects</p>
+      </div>
+    </>
   );
 };
 
