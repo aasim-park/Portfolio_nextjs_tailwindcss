01e12edcb9b56be9f389767622f4f24afff73fdd
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-02-21 11:02:14 +0530

    updated footer

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>Copyright 2022 </p>
+    <footer className="flex justify-center p-5 border-y-4">
+      <p>Copyright &#169; 2022 </p>
     </footer>
   );
 };
