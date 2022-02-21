5b7cc5f823592af5110afca1a69239e0a15a7ccc
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-02-21 10:27:11 +0530

    basic tailwind setup done for NAVBAR

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -2,19 +2,23 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav>
-      <div>
-        <a>Logo</a>
+    <nav className="flex flex-row">
+      {/* Logo */}
+      <div className="flex justify-center w-1/5 h-8 m-4">
+        <a className="text-2xl">Logo</a>
       </div>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/Projects">
-        <a>Projects</a>
-      </Link>
-      <Link href="/Contact">
-        <a>Contact</a>
-      </Link>
+      {/* right side navigation bar */}
+      <div className="flex justify-end w-4/5 gap-10 pr-8 m-4">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/Projects">
+          <a>Projects</a>
+        </Link>
+        <Link href="/Contact">
+          <a>Contact</a>
+        </Link>
+      </div>
     </nav>
   );
 };
